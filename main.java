0be72226303f5225895db9a2e2542e83b7691e90
Khutjo MAPUTLA <kmaputla@c5r3s11.wethinkcode.co.za>
2019-06-20 18:24:29 +0200

    added a few classes AirCraft

--- main.java
+++ main.java
@@ -1,13 +1,71 @@
+import java.io.File; 
+import java.io.FileNotFoundException; 
+import java.util.*; 
+
+class AirCraftData {
+	private String AirCraftType;
+	private String AirCraftName;
+	private int Latitude;
+	private int Longitude;
+	private int Height;
+
+	public AirCraftData(){
+		AirCraftType = "NULL";
+		AirCraftName = "NULL";
+		Longitude = 0;
+		Latitude = 0;
+		Height = 0;
+	}
+
+	public void GetAirCraftData(String line) throws Exception{
+
+		String[] LineData = line.split(" ");
+		if (LineData.length != 5)
+			throw new IllegalStateException("Object not initialised");
+
+		AirCraftType = LineData[0];
+		AirCraftName = LineData[1];
+		Longitude = Integer.parseInt(LineData[2]);
+		Latitude = Integer.parseInt(LineData[3]);
+		Height = Integer.parseInt(LineData[4]);
+	}
+}
 
 class InFile {
 
-	String InFileName;
+	private String InFileName;
+	Stack<String> FileContent;
 
-public InFile(String infile){
-	InFileName = infile;
+
+
+
+private void ReadFromFile() throws Exception {
+
+File file = new File(InFileName);
+Scanner sc = new Scanner(file);
+List<AirCraftData> ListOfAirCraft;
+
+
+while (sc.hasNextLine()){
+	FileContent.push(sc.nextLine() + "\n");
+}
+	Iterator<String> hold = FileContent.iterator();
+	while(hold.hasNext()){
+	System.out.println(hold.next());
+	}
+//	System.out.println(FileContent); 
+
 }
 
-String get(){return InFileName;}
+public InFile(String infile) throws Exception
+{
+	InFileName = infile;
+	FileContent = new Stack<String>();
+	List<AirCraftData> ListOfAirCraft = new ArrayList<AirCraftData>();
+	ReadFromFile();
+ }
+
+public String get(){return InFileName;}
 
 }
 
@@ -16,14 +74,17 @@ public class main{
 
 public static void main (String[] args){
 
-
-//	System.out.println(args[0].isEmpty());
-if (args[0].isEmpty()){
+if (args.length == 0 || args[0].isEmpty()){
 	System.out.println("No File To Read Form");
 	return ;
 }
-	InFile FileToRead = new InFile(args[0]);
+try{InFile FileToRead = new InFile(args[0]);}
+catch (Exception e){
+	System.out.println("Unable to read from file");
+	return ;
+}
 	//	InFile FileToRead;// = InFile(args[0]);
+	//
 System.out.println("hello world!");
 //System.out.println(FileToRead.get());
 return;
